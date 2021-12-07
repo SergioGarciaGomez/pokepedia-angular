662f85c9eb2224751fce628ac2a4775cb69e1719
SergioGarciaGomez <sergicodcraft@gmail.com>
2021-12-07 15:40:33 +0100

    Comments

--- src/app/services/ability.service.ts
+++ src/app/services/ability.service.ts
@@ -1,26 +1,29 @@
 import { Injectable } from '@angular/core';
 import { Ability } from '../interfaces/ability';
+import { Pokemon } from '../interfaces/pokemon';
 
 // HTTP LEER JSON
 import { HttpClient } from '@angular/common/http';
-import { Pokemon } from '../interfaces/pokemon';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AbilityService {
 
+  // Array para cargar las habilidades
   abilities: Ability[] = []
 
   constructor(private http: HttpClient) {}
 
-  // PROMESA QUE ESPERA A LEER EL JSON Y METERLO EN LA VARIABLE POKEMON
+  // PROMESA QUE ESPERA A LEER EL JSON Y METERLO EN EL ARRAY ABILITIES
   async getAbilitiesFromJson() {
     this.abilities = await this.http.get('assets/abilities.json').toPromise() as Ability[]
     return this.abilities
   }
 
-
+  // Función que, según un Pokémon, coge su atributo abilityId (Los pokémons guardan solo el ID de las habilidades 
+  // que tienen), y según el ID de la habilidad, hace una búsqueda en el array de habilidades, buscando ese ID, 
+  // y haciendo un Array con las habilidades que coinciden con la abilityId del pokémon.
   public getAbilitiesFromPokemon(pokemon: Pokemon) {
 
     var arrayAbilityIndex = 0;
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,12 +1,10 @@
 import { Injectable } from '@angular/core';
+import { Pokemon } from '../interfaces/pokemon';
+import { Chart, registerables } from 'chart.js';
 
 // HTTP PARA LEER EL .JSON
 import { HttpClient } from '@angular/common/http';
 
-import { Pokemon } from '../interfaces/pokemon';
-
-import { Chart, registerables } from 'chart.js';
-import { AbilityService } from './ability.service';
 // Necesario para que el chart.js funcione
 Chart.register(...registerables);
 
@@ -17,7 +15,7 @@ export class PokemonService {
 
   pokemons: Pokemon[] = []
 
-  constructor(private http: HttpClient, private abilityService: AbilityService) {}
+  constructor(private http: HttpClient) {}
 
   // PROMESA QUE ESPERA A LEER EL JSON Y METERLO EN LA VARIABLE POKEMON
   async getPokemonsFromJson() {
@@ -27,14 +25,17 @@ export class PokemonService {
     return this.pokemons
   }
 
-  ascIdFilter() {
+  // Ordena el array de pokemons por ID ascendente
+  public ascIdFilter() {
     return this.pokemons.sort((a, b) => a.id - b.id)
   }
 
+  // Ordena el array de pokemons por ID descendente
   public descIdFilter() {
     return this.pokemons.sort((a, b) => (b.id - a.id))
   }
 
+  // Ordena el array de pokemons por nombre ascendente
   public ascNameFilter() {
     this.pokemons.sort(function (a, b) {
       if (a.name < b.name) {
@@ -48,6 +49,7 @@ export class PokemonService {
     return this.pokemons
   }
 
+  // Ordena el array de pokemons por nombre descendente
   public descNameFilter() {
     this.pokemons.sort(function (a, b) {
       if (a.name < b.name) {
@@ -61,46 +63,48 @@ export class PokemonService {
     return this.pokemons
   }
 
-  // FUNCIÓN QUE DEVUELVE UN POKÉMON QUE, PASÁNDOLE SU ID, DEVUELVE LOS DATOS DEL POKEMON CON DICHO ID
+  // Función que devuelve un Pokémon en función de su ID
   public getPokemon(id: number): Pokemon {
     return { ...this.pokemons.filter(pokemon => pokemon.id === id)[0] }
   }
 
-  // Coge el valor más pequeño dentro del array y devuelve el pokemon con ese Id, para devolver el primero de la
-  // lista
+  // Hace un array de los IDs, coge el valor más pequeño dentro de esos ID's y devuelve el pokemon con ese Id, 
+  // para devolver el primero de la lista
   public getFirstPokemon(): Pokemon {
-    let primerId = Math.min(...this.pokemons.map(function(o){ return o.id; }))
+    let primerId = Math.min(...this.pokemons.map(function(object){ return object.id; }))
     return this.getPokemon(primerId)
   }
 
+  // Hace un array de los IDs, coge el valor más grande dentro de esos ID's y devuelve el pokemon con ese Id, 
+  // para devolver el último de la lista
   public getLastPokemon(): Pokemon {
     let ultimoId = Math.max(...this.pokemons.map(function(o){ return o.id; }))
     return this.getPokemon(ultimoId)
   }
 
+  // Función que saca la posición anterior a el ID que pasámos por parámetro en el array de pokemons, a partir de 
+  // un array auxiliar con los ID's.
   public getPreviousPokemon(id: number): Pokemon {
 
     // Obtengo un array de los id
     const arrayId = this.pokemons.map(array => array.id)
     let position = 0
-    
-    // En un bucle, si el id es mayor que el id de X posición, significa que aún no hemos llegado a el id
-    // del Pokémon que pasamos por parámetro, y eso es lo que queremos, sacar la posición anterior y posterior
-    // a ese id. En este caso queremos sacar la posición siguiente, porque queremos sacar el anterior Pokémon,
-    // así que a la posición final le hacemos - 1
+
     for (let x = 0; x < arrayId.length; x++) {
       if (id > arrayId[x]) {
         position++
       }
     }
 
-    // Igualo el Id a la posicion del array, donde estaba el primer valor, justo antes de encontrarse con el Id
-    // original
+    // Igualo el nuevo Id a la posicion anterior del array, justo antes de encontrarse con el Id pasado 
+    // por parámetro [POSICION - 1]
     let newId = arrayId[position - 1]
     
     return this.getPokemon(newId)
   }
 
+  // Función que saca la posición siguiente a el ID que pasámos por parámetro en el array de pokemons, a partir de 
+  // un array auxiliar con los ID's.
   public getNextPokemon(id: number): Pokemon {
 
     // Obtengo un array de los id
@@ -115,8 +119,8 @@ export class PokemonService {
       }
     }
 
-    // Igualo el Id a la posicion del array, donde estaba el primer valor, justo antes de encontrarse con el Id
-    // original
+    // Igualo el nuevo Id a la posicion siguiente del array, justo antes de encontrarse con el Id pasado 
+    // por parámetro [POSICION + 1]
     let newId = arrayId[position + 1]
     
     return this.getPokemon(newId)
